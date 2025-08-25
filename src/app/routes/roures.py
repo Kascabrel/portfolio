86643ee711df263125d21a0a9726921342132730
Kascabrel <107616396+Kascabrel@users.noreply.github.com>
2025-08-25 14:15:15 +0200

    set an active style to the current page on the navigation_bar_item

--- src/app/routes/roures.py
+++ src/app/routes/roures.py
@@ -1,4 +1,4 @@
-from flask import redirect, url_for, render_template, request, flash
+from flask import redirect, url_for, render_template, request, flash, session
 from flask import Blueprint
 
 portfolio = Blueprint('portfolio', __name__)
@@ -11,18 +11,20 @@ def index():
 
 @portfolio.route('/home')
 def home():
-    # return render_template(welcome.html)
-    return render_template('index.html')
+    if not session.get('current_page'):
+        return render_template('index.html', current_page='home')
+    return render_template('index.html', current_page=session.get('current_page'))
 
 
 @portfolio.route('/about')
 def about():
-    return redirect( url_for("portfolio.home") + "#about")
+    session['current_page'] = 'about'
+    return redirect(url_for("portfolio.home") + "#about")
 
 
 @portfolio.route('/projects')
 def projects():
-    return render_template("project.html")
+    return render_template("project.html", current_page='projects')
 
 
 @portfolio.route("/contact", methods=["GET", "POST"])
@@ -32,20 +34,21 @@ def contact():
         email = request.form["email"]
         message = request.form["message"]
 
-        # Ici tu peux envoyer un email ou stocker le message en DB
+        # send an email or store the message into the database
         print(f"New message from {name} ({email}): {message}")
 
         flash("Thank you for your message! I will get back to you soon.", "success")
+        session['current_page'] = 'contacts'
         return redirect(url_for("portfolio.contact"))
 
-    return render_template("contact.html")
+    return render_template("contact.html", current_page="contact")
 
 
 @portfolio.route('/skills')
 def skills():
-    return render_template("skills.html")
+    return render_template("skills.html", current_page='skills')
 
 
 @portfolio.route('/experience')
 def experience():
-    return render_template("experience.html")
+    return render_template("experience.html", current_page='experience')
